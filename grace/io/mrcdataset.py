f114dfeda487ee11a4727244df22dda8339bef40
Chris-Soelistyo <christopher.soelistyo.15@ucl.ac.uk>
2023-04-21 13:16:01 +0100

    added imagegraphdataset class

---
+++ grace/io/mrcdataset.py
@@ -0,0 +1,73 @@
+from typing import Tuple, Callable
+import numpy.typing as npt
+
+import os
+
+import mrcfile
+
+from ..io import read_graph
+
+import torch
+from torch.utils.data import Dataset
+
+from pathlib import Path
+
+class ImageGraphDataset(Dataset):
+    """Creating a Torch dataset from an image directory and
+    annotation (.grace file) directory.
+
+    Parameters
+    ----------
+    imagepath: str
+        Directory of the image files
+    gracepath: str
+        Directory of the annotation (.grace) files
+    image_reader_fn: Callable
+        Function to read images from image filenames
+    transform : Callable
+        Transformation added to the images
+    target_transform : Callable
+        Transformation added to the targets (graph data)
+    """
+
+    def __init__(
+        self,
+        image_path: os.PathLike,
+        grace_path: os.PathLike,
+        image_reader_fn: Callable,
+        *,
+        transform: Callable = lambda x: x,
+        target_transform: Callable = lambda x: x,
+    ) -> None:
+
+        self.image_fns = list(image_path.iterdir())
+        self.grace_fns = list(grace_path.iterdir())
+        self.image_reader_fn = image_reader_fn
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self) -> int:
+        return len(self.image_fns)
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, dict]:
+
+        img_path = self.image_fns[idx]
+        grace_path = self.grace_fns[idx]
+
+        image = torch.tensor(self.image_reader_fn(img_path), dtype=torch.float32)
+        grace_dataset = read_graph(grace_path)
+
+        target = {}
+        target["graph"] = grace_dataset.graph
+        target["metadata"] = grace_dataset.metadata
+        assert img_path.stem == target["metadata"]["image_filename"]
+
+        image = self.transform(image)
+        target = self.target_transform(target)
+
+        return image, target
+    
+def mrc_reader(fn: os.PathLike) -> npt.NDArray:
+    with mrcfile.open(fn, "r") as mrc:
+        image_data = mrc.data.astype(int)
+    return image_data
