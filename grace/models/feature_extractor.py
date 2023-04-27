3b4aabb2e942791f718294c3f62d70c0e5dba437
Chris-Soelistyo <christopher.soelistyo.15@ucl.ac.uk>
2023-04-27 10:39:14 +0100

    added feature extractor

---
+++ grace/models/feature_extractor.py
@@ -0,0 +1,106 @@
+from typing import Any, Dict, Tuple, Callable
+
+import torch
+
+from torchvision.models import resnet152, ResNet152_Weights
+from torchvision.transforms import Lambda, Normalize, Compose, RandomApply
+
+from grace.base import GraphAttrs
+from grace.utils.augment_image import RandomEdgeCrop
+
+
+def resnet() -> torch.nn.Module:
+    """Returns resnet152, not including the final layer."""
+    resnet152_model = resnet152(weights=ResNet152_Weights.DEFAULT)
+    modules = list(resnet152_model.children())[:-1]
+    resnet152_model = torch.nn.Sequential(*modules)
+    for p in resnet152_model.parameters():
+        p.requires_grad = False
+
+    return resnet152_model
+
+
+default_transforms = Compose(
+    [
+        Normalize(mean=[0.0], std=[1.0]),
+        Lambda(lambda x: x.expand(1, 3, 224, 224)),
+    ]
+)
+default_augmentations = RandomApply([RandomEdgeCrop(max_fraction=0.1)], p=0.3)
+
+
+class FeatureExtractor(torch.nn.Module):
+    """A feature extraction model. Crops bounding boxes according to
+    graph coordinates then calculates features with a specified model.
+
+    Parameters
+    ----------
+    bbox_size : Tuple[int]
+        Size the bounding boxes to be extracted, centered
+        on the x-y coordinates of each node; (W, H)
+    model : Callable
+        Feature extractor model or function that maps a tensor
+        to a tensor
+    transforms : Callable
+        Series of transforms to apply to the bbox image before
+        feature extraction
+    augmentations : Callable
+        Series of augmentations to apply to the bbox image
+        during training
+    """
+
+    def __init__(
+        self,
+        *,
+        bbox_size: Tuple[int] = (224, 224),
+        model: Callable = resnet(),
+        transforms: Callable = default_transforms,
+        augmentations: Callable = default_augmentations,
+    ) -> None:
+        super(FeatureExtractor, self).__init__()
+        self.bbox_size = bbox_size
+        self.model = model
+        self.transforms = transforms
+        self.augmentations = augmentations
+
+    def forward(
+        self,
+        image: torch.Tensor,
+        graph: Dict[str, Any],
+    ) -> Tuple[torch.Tensor, dict]:
+        """Adds a feature vector to each graph node.
+
+        Parameters
+        ----------
+        image : torch.Tensor
+            Image of shape (W,H), (C,W,H) or (B,C,W,H)
+        graph : dict
+            Dictionary with key "graph" that accesses the networkx graph
+        """
+
+        if image.ndim < 4:
+            image = image[[None] * (4 - len(image.size()))]
+
+        for node in graph["graph"].nodes.data():
+            x, y = int(node[1][GraphAttrs.NODE_X]), int(
+                node[1][GraphAttrs.NODE_Y]
+            )
+            bbox_image = image[
+                ...,
+                x
+                - int(self.bbox_size[0] / 2) : x
+                + int(self.bbox_size[0] / 2),
+                y
+                - int(self.bbox_size[1] / 2) : y
+                + int(self.bbox_size[1] / 2),
+            ]
+            bbox_image = self.transforms(bbox_image)
+
+            if self.training:
+                bbox_image = self.augmentations(bbox_image)
+
+            features = self.model(bbox_image)
+            node[1][GraphAttrs.NODE_FEATURES] = features.squeeze()
+
+        image = image.squeeze()
+        return image, graph
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,14 @@
 from grace.models.classifier import GCN
+from grace.models.feature_extractor import FeatureExtractor
 
+import torch
 import pytest
+
+import numpy as np
+
+from _utils import random_image_and_graph
+
+from grace.base import GraphAttrs
 
 
 @pytest.mark.parametrize("input_dims", [1, 2, 4])
@@ -18,3 +26,47 @@ def test_model_building(input_dims, embedding_dims, output_dims):
     assert model.conv1.in_channels == input_dims
     assert model.linear.in_features == embedding_dims
     assert model.linear.out_features == output_dims
+
+
+@pytest.mark.parametrize(
+    "bbox_size, model",
+    [
+        ((2, 2), lambda x: torch.sum(x)),
+        ((3, 3), lambda x: torch.mean(x)),
+    ],
+)
+class TestFeatureExtractor:
+    @pytest.fixture
+    def vars(self, bbox_size, model, default_rng):
+        image, graph = random_image_and_graph(default_rng)
+        return {
+            "extractor": FeatureExtractor(
+                bbox_size=bbox_size,
+                model=model,
+                transforms=lambda x: x,
+                augmentations=lambda x: x,
+            ),
+            "image": image,
+            "graph": graph,
+        }
+
+    def test_feature_extractor_forward(self, bbox_size, model, vars):
+        image_out, target_out = vars["extractor"](
+            vars["image"], {"graph": vars["graph"]}
+        )
+        graph_out = target_out["graph"]
+
+        assert np.array_equal(vars["image"], image_out.numpy())
+
+        for node in graph_out.nodes.data():
+            x, y = int(node[1][GraphAttrs.NODE_X]), int(
+                node[1][GraphAttrs.NODE_Y]
+            )
+            features = node[1][GraphAttrs.NODE_FEATURES]
+
+            assert features == model(
+                vars["image"][
+                    x - bbox_size[0] : x + bbox_size[0],
+                    y - bbox_size[1] : y + bbox_size[1],
+                ]
+            )
