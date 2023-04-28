2c552f43a6ecf7885a088b636b61846e809f3a89
Chris-Soelistyo <christopher.soelistyo.15@ucl.ac.uk>
2023-04-28 14:23:30 +0100

    changed resnet function to general function

--- grace/models/feature_extractor.py
+++ grace/models/feature_extractor.py
@@ -15,15 +15,26 @@ from grace.base import GraphAttrs
 from grace.utils.augment_image import RandomEdgeCrop
 
 
-def resnet() -> torch.nn.Module:
-    """Returns resnet152, not including the final layer."""
-    resnet152_model = resnet152(weights=ResNet152_Weights.DEFAULT)
-    modules = list(resnet152_model.children())[:-1]
-    resnet152_model = torch.nn.Sequential(*modules)
-    for p in resnet152_model.parameters():
+def classifier_to_feature_extractor(classifier: torch.nn.Module) -> torch.nn.Module:
+    """Takes a pre-trained classification model and returns a copy in which
+    the final (classification) layer has been removed.
+    
+    Parameters
+    ----------
+    classifier : torch.nn.Module
+        Classifier model
+
+    Returns
+    -------
+    extractor : torch.nn.Module
+        Feature extractor model
+    """
+    modules = list(classifier.children())[:-1]
+    extractor = torch.nn.Sequential(*modules)
+    for p in extractor.parameters():
         p.requires_grad = False
 
-    return resnet152_model
+    return extractor
 
 
 default_transforms = Compose(
@@ -60,7 +71,7 @@ class FeatureExtractor(torch.nn.Module):
         self,
         *,
         bbox_size: Tuple[int] = (224, 224),
-        model: Callable = resnet(),
+        model: Callable = classifier_to_feature_extractor(resnet152(ResNet152_Weights)),
         transforms: Callable = default_transforms,
         augmentations: Callable = default_augmentations,
     ) -> None:
