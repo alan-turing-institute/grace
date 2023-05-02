e36fc7bd618244f3207153ba89b0822a187e158e
Chris-Soelistyo <christopher.soelistyo.15@ucl.ac.uk>
2023-05-02 13:48:28 +0100

    add edge and node GCN

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,36 +1,41 @@
 import torch
-import pytest
 
 from grace.base import Annotation
 from grace.models.train import edge_criterion
-from grace.models.classifier import GCN
+
 
 def test_edge_criterion_masks_unknown_ground_truth():
-
     masked_class = Annotation.UNKNOWN
 
-    embed = torch.tensor([
-        [-1.,-1.],
-        [1.,1.],
-    ], dtype=torch.float32)
-
-    target = torch.tensor([
-        Annotation.TRUE_NEGATIVE,
-        Annotation.TRUE_POSITIVE,
-        Annotation.TRUE_NEGATIVE,
-        Annotation.UNKNOWN,
-    ], dtype=torch.float32)
-
-    edge_index = torch.tensor([
-        [0,0,0,0],
-        [1,1,1,1],
-    ], dtype=torch.int32)
-
-    assert (
-        edge_criterion(embed, target, edge_index, masked_class)
-        == edge_criterion(embed, target[:-1], edge_index[:,:-1], masked_class)
+    embed = torch.tensor(
+        [
+            [-1.0, -1.0],
+            [1.0, 1.0],
+        ],
+        dtype=torch.float32,
     )
-    assert (
-        edge_criterion(embed, target, edge_index, masked_class)
-        != edge_criterion(embed, target[1:], edge_index[:,1:], masked_class)
+
+    target = torch.tensor(
+        [
+            Annotation.TRUE_NEGATIVE,
+            Annotation.TRUE_POSITIVE,
+            Annotation.TRUE_NEGATIVE,
+            Annotation.UNKNOWN,
+        ],
+        dtype=torch.float32,
     )
+
+    edge_index = torch.tensor(
+        [
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+        ],
+        dtype=torch.int32,
+    )
+
+    assert edge_criterion(
+        embed, target, edge_index, masked_class
+    ) == edge_criterion(embed, target[:-1], edge_index[:, :-1], masked_class)
+    assert edge_criterion(
+        embed, target, edge_index, masked_class
+    ) != edge_criterion(embed, target[1:], edge_index[:, 1:], masked_class)
