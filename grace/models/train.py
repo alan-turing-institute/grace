a25c7c4bb93e157515ed71550c30afe26c8b4ed4
Chris-Soelistyo <christopher.soelistyo.15@ucl.ac.uk>
2023-05-03 16:45:56 +0100

    add logger

--- grace/models/train.py
+++ grace/models/train.py
@@ -42,77 +42,88 @@ def train_model(
     )
 
     def calculate_batch_metrics(data: torch_geometric.data.Data) -> Tuple[torch.Tensor]:
-        out_x, out_embedding = model(data.x, data.edge_index, data.batch)
+        node_x, edge_x = model(data.x, data.edge_index, data.batch)
 
-        loss_node = node_criterion(out_embedding, data.y)
-        loss_edge = edge_criterion(out_x, data.edge_label)
+        loss_node = node_criterion(node_x, data.y)
+        loss_edge = edge_criterion(edge_x, data.edge_label)
         loss = loss_node + loss_edge
 
-        
+        pred_node = node_x.argmax(dim=-1)
+        pred_edge = edge_x.argmax(dim=-1)
+        correct_nodes = (pred_node == data.y).sum()
+        correct_edges = (pred_edge == data.edge_label).sum()
 
         return {
             "loss": loss,
             "loss_node": loss_node,
             "loss_edge": loss_edge,
+            "correct_nodes": correct_nodes,
+            "correct_edges": correct_edges,
+            "num_nodes": len(node_x),
+            "num_edges": torch.numel(data.edge_label),
         }
 
     def train():
         model.train()
 
         for data in train_loader:
-            loss, loss_node, loss_edge = calculate_batch_metrics(data)
+            node_x, edge_x = model(data.x, data.edge_index, data.batch)
+
+            loss_node = node_criterion(node_x, data.y)
+            loss_edge = edge_criterion(edge_x, data.edge_label)
+            loss = loss_node + loss_edge
 
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-
-        return {
-            "loss": loss,
-            "loss_node": loss_node,
-            "loss_edge": loss_edge,
-        }
     
     def test(loader):
         """Evaluates the GCN on node classification."""
         model.eval()
 
-        correct_nodes = 0
-        correct_edges = 0
-        num_edges = 0
+        epoch_metrics = {}
 
         for data in loader:
-            out_x, out_embedding = model(data.x, data.edge_index, data.batch)
+            metrics = calculate_batch_metrics(data)
 
-            # node
-            pred = out_x.argmax(
-                dim=1
-            )  # Use the class with highest probability.
-            correct_nodes += int((pred == data.y).sum())
+            if epoch_metrics:
+                for key in epoch_metrics:
+                    epoch_metrics[key] += metrics[key]
+            else:
+                epoch_metrics = metrics
 
-            # edge
-            src, dst = data.edge_index
-            edge_score = (out_embedding[src] * out_embedding[dst]).sum(dim=-1)
-            num_edges += edge_score.size(dim=0)
-            correct_edges += int((torch.round(pred) == data.edge_label).sum())
-
-        return correct_nodes / len(loader.dataset), correct_edges / num_edges
+        return {
+            "loss": epoch_metrics["loss"],
+            "loss_node": epoch_metrics["loss_node"],
+            "loss_edge": epoch_metrics["loss_edge"],
+            "acc_node": epoch_metrics["correct_nodes"] / epoch_metrics["num_nodes"],
+            "acc_edge": epoch_metrics["correct_edges"] / epoch_metrics["num_edges"],
+        }
 
     for epoch in range(1, epochs):
-        loss, loss_node, loss_edge = train()
-        train_acc_node, train_acc_edge = test(train_loader)
-        test_acc_node, test_acc_edge = test(test_loader)
-        print(
+        train()
+        train_metrics = test(train_loader)
+        test_metrics = test(test_loader)
+        '''print(
             f"Epoch: {epoch:03d}, Loss: {loss:.4f},"
             f" Node Loss: {loss_node:.4f}, Edge Loss: {loss_edge:.4f},"
             f" Train Acc (Node): {train_acc_node:.4f},"
             f" Train Acc (Edge): {train_acc_edge:.4f},"
             f" Test Acc (Node): {test_acc_node:.4f},"
             f" Test Acc (Edge): {test_acc_edge:.4f}"
-        )
+        )'''
 
-        writer.add_scalars('Loss/train', {'total': loss,
-                                          'node': loss_node,
-                                          'edge': loss_edge,}, epoch)
-        writer.add_scalars('Loss/train', {'total': loss,
-                                          'node': loss_node,
-                                          'edge': loss_edge,}, epoch)
+        writer.add_scalars('Loss/train', {'total': train_metrics["loss"],
+                                          'node': train_metrics["loss_node"],
+                                          'edge': train_metrics["loss_edge"],}, 
+                                          epoch)
+        writer.add_scalars('Loss/test', {'total': test_metrics["loss"],
+                                          'node': test_metrics["loss_node"],
+                                          'edge': test_metrics["loss_edge"],}, 
+                                          epoch)
+        writer.add_scalars('Accuracy/train', {'node': train_metrics["acc_node"],
+                                            'edge': train_metrics["acc_edge"],}, 
+                                            epoch)
+        writer.add_scalars('Accuracy/test', {'node': test_metrics["acc_node"],
+                                            'edge': test_metrics["acc_edge"],}, 
+                                            epoch)
