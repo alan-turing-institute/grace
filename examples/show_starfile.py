27108f71425de9dfc1477ca317b72d45e120eb1f
mooniean <bmbcgomes@gmail.com>
2023-08-16 15:56:00 +0100

    added show_starfile.py

---
+++ examples/show_starfile.py
@@ -0,0 +1,87 @@
+import napari
+
+import mrcfile
+import numpy as np
+
+from grace.base import GraphAttrs
+from grace.io import starfile, core
+from pathlib import Path
+
+#
+path_input = input("Input data path:")
+
+if len(path_input) < 1:
+    DATA_PATH = Path("/Users/bcostagomes/Documents/2dEM_images/annotation_dataset")
+else:
+    DATA_PATH = Path(path_input)
+
+
+IMAGE_PATH = DATA_PATH / "images"
+STAR_PATH = DATA_PATH / "detections"
+
+# Makes the grace files/directory from the starfile folder, for every starfile. 
+starfile.mkdir_grace_from_star(STAR_PATH)
+
+# Gets list of available files in the image
+name_list = [f for f in IMAGE_PATH.iterdir() if f.is_file()]
+
+
+GRACE_PATH = DATA_PATH / "grace"
+
+# Runs only the first one
+# TODO: run a batch
+
+FILE_STEM = Path(
+    name_list[0].stem
+)
+
+print(FILE_STEM)
+
+
+
+IMAGE = IMAGE_PATH / FILE_STEM.with_suffix(".mrc")
+DETECTION = GRACE_PATH / FILE_STEM.with_suffix(".grace")
+
+with mrcfile.open(IMAGE, "r") as mrc:
+    image_data = mrc.data.astype(int)
+
+grace_file = core.GraceFile(DETECTION)
+grace_file = grace_file.read()
+
+graph = grace_file.graph
+
+# make points from node attributes
+points = []
+for i in range(0, len(graph.nodes)):
+    x,y = graph.nodes[i]['x'],graph.nodes[i]['y']
+    points.append([y,x])
+
+
+features = None
+
+data_name = f"{IMAGE_PATH.stem}"
+
+viewer = napari.Viewer()
+img_layer = viewer.add_image(image_data, name=data_name)
+pts_layer = viewer.add_points(
+    points,
+    features=features,
+    size=32,
+    name=f"nodes_{data_name}",
+    # text= {
+    #     "string": "{confidence:.2f}",
+    #     "size": 8,
+    #     "color": "r",
+    # }
+)
+
+
+_, widget = viewer.window.add_plugin_dock_widget(
+    plugin_name="grace", widget_name="GRACE"
+)
+
+if __name__ == "__main__":
+    # The napari event loop needs to be run under here to allow the window
+    # to be spawned from a Python script
+    napari.run()
+
