5f8dd85645225f60ed05c7a57d5a1e00659a76ce
KristinaUlicna <kulicna@turing.ac.uk>
2023-09-19 12:07:38 +0100

    Add a subgraph plotter from local node

--- grace/visualisation/subgraph.py
+++ grace/visualisation/subgraph.py
@@ -1,77 +1,187 @@
 import numpy as np
+import networkx as nx
 import matplotlib
 import torch_geometric
+import numpy.typing as npt
+
+from grace.base import GraphAttrs
+from grace.models.datasets import _pos, _edge_index
 
 
-COLOR_MAPPING = {
-    0: "dodgerblue",  # "magenta",
-    1: "firebrick",  # "green",
-    2: "grey",
-}
-
-
-# TODO: implement a function that processes subgraph directly from G:
-# def process_subgraph_data(sub_graph: nx.Graph | torch_geometric.data.Data):
+COLOR_MAPPING = {0: "royalblue", 1: "firebrick", 2: "grey"}
+STYLE_MAPPING = {0: "dashed", 1: "solid", 2: "dashed"}
 
 
 def plot_subgraph_geometry(
-    sub_graph: torch_geometric.data.Data,
+    sub_graph: torch_geometric.data.Data, **kwargs
+) -> tuple[npt.NDArray]:
+    # Extract data from subgraph:
+    node_positions = sub_graph.edge_attr
+    edge_indices = sub_graph.edge_index
+    node_true_labels = sub_graph.y
+    edge_true_labels = sub_graph.edge_label
+
+    # Perform dimensionality checks:
+    assert node_positions.shape[0] == len(node_true_labels)
+    assert edge_indices.shape[-1] == len(edge_true_labels)
+
+    # Plot the subgraph:
+    ax = plot_subgraph_coordinates(
+        node_positions,
+        edge_indices,
+        node_true_labels,
+        edge_true_labels,
+        **kwargs,
+    )
+    return ax
+
+
+def plot_local_node_geometry(
+    G: nx.Graph, node_idx: int, **kwargs
+) -> tuple[npt.NDArray]:
+    # Define a subgraph with 1-hop connectivity:
+    sub_graph = nx.ego_graph(G, node_idx, radius=1)
+    central_node = np.array(
+        [
+            G.nodes[node_idx][GraphAttrs.NODE_X],
+            G.nodes[node_idx][GraphAttrs.NODE_Y],
+        ]
+    )
+    node_positions = _pos(sub_graph).numpy() - central_node
+    edge_indices = _edge_index(sub_graph)
+
+    # Extract data from subgraph:
+    node_true_labels = np.stack(
+        [
+            n[GraphAttrs.NODE_GROUND_TRUTH]
+            for _, n in sub_graph.nodes(data=True)
+        ]
+    )
+    edge_true_labels = np.stack(
+        [
+            e[-1][GraphAttrs.EDGE_GROUND_TRUTH]
+            for e in sub_graph.edges(data=True)
+        ]
+    )
+
+    # Optional: node & edge predictions:
+    node_pred_labels = [
+        n[-1][GraphAttrs.NODE_PREDICTION][0]
+        if GraphAttrs.NODE_PREDICTION in n[-1]
+        else None
+        for n in sub_graph.nodes(data=True)
+    ]
+    if all([v is None for v in node_pred_labels]):
+        node_pred_labels = None
+    else:
+        node_pred_labels = np.stack(node_pred_labels)
+        assert node_pred_labels.shape == node_true_labels.shape
+
+    edge_pred_labels = [
+        e[-1][GraphAttrs.EDGE_PREDICTION][0]
+        if GraphAttrs.EDGE_PREDICTION in e[-1]
+        else None
+        for e in sub_graph.edges(data=True)
+    ]
+    if all([v is None for v in edge_pred_labels]):
+        edge_pred_labels = None
+    else:
+        edge_pred_labels = np.stack(edge_pred_labels)
+        assert edge_pred_labels.shape == edge_true_labels.shape
+
+    # Perform dimensionality checks:
+    assert node_positions.shape[0] == len(node_true_labels)
+    assert edge_indices.shape[-1] == len(edge_true_labels)
+
+    # Plot the subgraph:
+    ax = plot_subgraph_coordinates(
+        node_positions,
+        edge_indices,
+        node_true_labels,
+        edge_true_labels,
+        node_pred_labels,
+        edge_pred_labels,
+        **kwargs,
+    )
+    return ax
+
+
+def plot_subgraph_coordinates(
+    node_positions: npt.NDArray,
+    edge_indices: npt.NDArray,
+    node_true_labels: npt.NDArray,
+    edge_true_labels: npt.NDArray,
+    node_pred_labels: npt.NDArray = None,
+    edge_pred_labels: npt.NDArray = None,
     *,
     title: str = "",
     color_mapping: dict[int, str] = None,
+    style_mapping: dict[int, str] = None,
     ax: matplotlib.axes = None,
 ) -> matplotlib.axes:
-    # Extract data from subgraph:
-    node_positions = sub_graph.edge_attr.numpy()
-    node_GT_labels = sub_graph.y.numpy()
-    edge_GT_labels = sub_graph.edge_label.tolist()
-    edge_indices = sub_graph.edge_index.numpy()
-    assert node_positions.shape[0] == len(node_GT_labels)
-    assert edge_indices.shape[-1] == len(edge_GT_labels)
-
     # Define color mapping & plot:
     color_mapping = (
         color_mapping if color_mapping is not None else COLOR_MAPPING
+    )
+    style_mapping = (
+        style_mapping if style_mapping is not None else STYLE_MAPPING
     )
 
     # Plot the edges underneath:
     for e_idx in range(edge_indices.shape[-1]):
         src, dst = edge_indices[:, e_idx]
         st, en = node_positions[src], node_positions[dst]
-        true_color = color_mapping[edge_GT_labels[e_idx]]
+        true_color = color_mapping[edge_true_labels[e_idx].item()]
         ax.plot(
             [st[0], en[0]], [st[1], en[1]], lw=5, ls="solid", c=true_color
         )  # true
 
-        # TODO: implement non-random predictions:
-        pred_style = "dashed" if np.random.randint(2) == 0 else "solid"
-        ax.plot(
-            [st[0], en[0]], [st[1], en[1]], lw=2, ls=pred_style, c="black"
-        )  # pred
+        # Visualise GCN predictions for edges, if any:
+        if edge_pred_labels is not None:
+            pred_style = "solid" if edge_pred_labels[e_idx] == 1 else "dashed"
+            ax.plot(
+                [st[0], en[0]], [st[1], en[1]], lw=2, ls=pred_style, c="black"
+            )  # pred
 
     # Plot the scatter on top:
-    node_color = [color_mapping[GT] for GT in node_GT_labels]
-    # TODO: implement non-random predictions:
-    node_style = [
-        "solid" if np.random.randint(2) == 0 else "dashed"
-        for _ in range(len(node_GT_labels))
-    ]
-    ax.scatter(
-        x=node_positions[:, 0],
-        y=node_positions[:, 1],
-        s=200,
-        c=node_color,
-        ls=node_style,
-        linewidths=2,
-        edgecolors="k",
-        zorder=edge_indices.shape[-1] + 1,
-    )
+    for n_idx in range(node_positions.shape[0]):
+        node_color = color_mapping[node_true_labels[n_idx].item()]
+        ax.scatter(
+            x=node_positions[n_idx, 0],
+            y=node_positions[n_idx, 1],
+            s=200,
+            c=node_color,
+            zorder=edge_indices.shape[-1] + n_idx + 1,
+        )
+
+        # Visualise GCN predictions for nodes, if any:
+        if node_pred_labels is not None:
+            node_style = "solid" if node_pred_labels[n_idx] == 1 else "dashed"
+            ax.scatter(
+                x=node_positions[n_idx, 0],
+                y=node_positions[n_idx, 1],
+                s=200,
+                ls=node_style,
+                linewidths=2,
+                facecolors="none",
+                edgecolors="k",
+                zorder=edge_indices.shape[-1] + n_idx + 2,
+            )
 
     # Fake a legend:
-    [
-        ax.scatter(x=[], y=[], c=color_mapping[i], label=f"Label '{i}'")
-        for i in range(len(color_mapping))
-    ]
+    for i in range(len(color_mapping)):
+        ax.scatter(x=[], y=[], c=color_mapping[i], label=f"GT Label '{i}'")
+        if i > 1:
+            continue
+        ax.scatter(
+            x=[],
+            y=[],
+            facecolors="none",
+            edgecolors="k",
+            ls=style_mapping[i],
+            label=f"Prediction '{i}'",
+        )
+
     ax.legend(
         loc="upper center",
         bbox_to_anchor=(0.5, -0.05),
