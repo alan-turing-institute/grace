69b385d88e39dd471e4cfc2bb8be0793ce648dd3
KristinaUlicna <kulicna@turing.ac.uk>
2023-09-29 14:26:30 +0100

    Click command on napari annotator

--- examples/show_data.py
+++ examples/show_data.py
@@ -7,12 +7,7 @@ import numpy as np
 from pathlib import Path
 
 from grace.base import GraphAttrs
-from grace.io.image_dataset import (
-    FILETYPES,
-    mrc_reader,
-    tiff_reader,
-    png_reader,
-)
+from grace.io.image_dataset import FILETYPES
 
 
 # Define a click command to input the file name directly:
@@ -23,34 +18,34 @@ def run_napari_annotator(image_path=str) -> None:
     # Use identical naming convention for files & specify whole path to mrc file:
     # e.g. /Users/kulicna/Desktop/dataset/shape_squares/MRC_Synthetic_File_000.mrc
 
+    # Process the image data + load nodes:
     suffix = image_path.split(".")[-1]
     assert suffix in FILETYPES, f"Choose these filetypes: {FILETYPES.keys()}"
-    IMAGE_PATH = Path(image_path)
 
-    if suffix == "mrc":
-        image_data = mrc_reader(IMAGE_PATH)
-    elif suffix == "png":
-        image_data = png_reader(IMAGE_PATH)
-    elif suffix == "tiff":
-        image_data = tiff_reader(IMAGE_PATH)
+    image_reader = FILETYPES[suffix]
+    image_data = image_reader(Path(image_path))
 
     nodes_path = image_path.replace(".mrc", ".h5")
     nodes_data = pd.read_hdf(Path(nodes_path))
 
+    data_name = f"{Path(image_path).stem}"
+
+    # Start a napari window:
+    viewer = napari.Viewer()
+    mn, mx = np.min(image_data), np.max(image_data)
+    viewer.add_image(image_data, name=data_name, contrast_limits=(mn, mx))
+
+    # Locate the nodes as points:
     points = np.asarray(
         nodes_data.loc[:, [GraphAttrs.NODE_Y, GraphAttrs.NODE_X]]
     )
+    # Process the features information - TSNE:
     # features = {
     #     GraphAttrs.NODE_FEATURES:
     #     [np.squeeze(f.numpy()) for f in nodes_data.loc[:, "features"]]
     # }
     features = None
-    data_name = f"{IMAGE_PATH.stem}"
 
-    mn, mx = np.min(image_data), np.max(image_data)
-
-    viewer = napari.Viewer()
-    viewer.add_image(image_data, name=data_name, contrast_limits=(mn, mx))
     viewer.add_points(
         points, features=features, size=32, name=f"nodes_{data_name}"
     )
