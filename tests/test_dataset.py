84f019e1d7f65ac2318000a46c78d482a9483b1a
KristinaUlicna <kulicna@turing.ac.uk>
2023-10-03 18:35:01 +0100

    Modified tests based on updated classifier architecture

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,10 +1,7 @@
-import pytest
+import torch
+import networkx as nx
 
-from grace.base import GraphAttrs, Annotation
 from grace.io.image_dataset import ImageGraphDataset
-from grace.models.datasets import dataset_from_graph
-
-from _utils import random_image_and_graph
 
 
 def test_image_graph_dataset(mrc_image_and_annotations_dir):
@@ -16,63 +13,22 @@ def test_image_graph_dataset(mrc_image_and_annotations_dir):
         mrc_image_and_annotations_dir,
         image_filetype="mrc",
     )
-
-    # # all currently fail
-    # image, graph = dataset[0]
-
-    # assert isinstance(image, torch.Tensor)
-    # assert isinstance(graph, nx.Graph)
-
-    # assert image.shape == (1, 128, 128)
-    # assert graph.number_of_nodes() == 4
-
     assert len(dataset) == num_images
 
+    # unwrap sample image & target
+    image, target = dataset[0]
+    graph = target["graph"]
+    metadata = target["metadata"]
+    filename = metadata["image_filename"]
 
-def test_dataset_ignores_subgraph_if_all_edges_unknown(default_rng):
-    _, graph = random_image_and_graph(default_rng)
+    assert isinstance(image, torch.Tensor)
+    assert image.shape == (128, 128)  # 2D image
 
-    edge_update = [
-        (src, dst, {GraphAttrs.EDGE_GROUND_TRUTH: Annotation.UNKNOWN})
-        for src, dst in graph.edges
-    ]
-    graph.update(edges=edge_update)
-    # this action is not currently required since edges are by default UNKNOWN;
-    # however it enables testing of this condition should the default label be changed
+    assert isinstance(metadata, dict)
+    assert isinstance(filename, str)
 
-    assert dataset_from_graph(graph, mode="sub") == []
-
-
-@pytest.mark.parametrize("num_unknown", [7, 17])
-def test_dataset_ignores_subgraph_if_central_node_unknown(
-    num_unknown, default_rng
-):
-    num_nodes_total = 20
-    _, graph = random_image_and_graph(
-        default_rng,
-        num_nodes=num_nodes_total,
-    )
-
-    edge_update = [
-        (src, dst, {GraphAttrs.EDGE_GROUND_TRUTH: Annotation.TRUE_POSITIVE})
-        for src, dst in graph.edges
-    ]
-    graph.update(edges=edge_update)
-
-    node_update = [
-        (node, {GraphAttrs.NODE_GROUND_TRUTH: Annotation.UNKNOWN})
-        for node in list(graph.nodes)[:num_unknown]
-    ]
-    node_update += [
-        (node, {GraphAttrs.NODE_GROUND_TRUTH: Annotation.TRUE_POSITIVE})
-        for node in list(graph.nodes)[num_unknown:]
-    ]
-    graph.update(nodes=node_update)
-
-    assert (
-        len(dataset_from_graph(graph, mode="sub"))
-        == num_nodes_total - num_unknown
-    )
+    assert isinstance(graph, nx.Graph)
+    assert graph.number_of_nodes() == 4
 
 
 def test_dataset_only_takes_common_filenames(tmp_path):
--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,94 +1,11 @@
-from grace.models.classifier import GCNModel
-from grace.models.feature_extractor import FeatureExtractor
-
 import math
 import torch
 import pytest
 
-import networkx as nx
-
-from _utils import random_image_and_graph
-
 from grace.base import GraphAttrs, Annotation
-from grace.models.datasets import dataset_from_graph
+from grace.models.feature_extractor import FeatureExtractor
 
-
-@pytest.mark.parametrize("input_channels", [1, 2])
-@pytest.mark.parametrize("hidden_graph_channels", [[16, 4], [32, 8]])
-@pytest.mark.parametrize("hidden_dense_channels", [[16, 4], [32, 8]])
-@pytest.mark.parametrize("node_output_classes", [2, 4])
-@pytest.mark.parametrize("edge_output_classes", [2, 4])
-class TestGCN:
-    @pytest.fixture
-    def gcn(
-        self,
-        input_channels,
-        hidden_graph_channels,
-        hidden_dense_channels,
-        node_output_classes,
-        edge_output_classes,
-    ):
-        return GCNModel(
-            input_channels=input_channels,
-            hidden_graph_channels=hidden_graph_channels,
-            hidden_dense_channels=hidden_dense_channels,
-            node_output_classes=node_output_classes,
-            edge_output_classes=edge_output_classes,
-        )
-
-    def test_model_building(
-        self,
-        input_channels,
-        hidden_graph_channels,
-        hidden_dense_channels,
-        node_output_classes,
-        edge_output_classes,
-        gcn,
-    ):
-        """Test building the model with different dimensions."""
-
-        assert gcn.conv_layer_list[0].in_channels == input_channels
-
-        assert gcn.node_classifier.in_features == hidden_dense_channels[-1]
-        assert gcn.node_classifier.out_features == node_output_classes
-
-        assert gcn.edge_classifier.in_features == hidden_dense_channels[-1] * 2
-        assert gcn.edge_classifier.out_features == edge_output_classes
-
-    @pytest.mark.parametrize("num_nodes", [4, 5])
-    def test_output_sizes(
-        self,
-        input_channels,
-        hidden_graph_channels,
-        hidden_dense_channels,
-        node_output_classes,
-        edge_output_classes,
-        gcn,
-        num_nodes,
-        default_rng,
-    ):
-        _, graph = random_image_and_graph(
-            default_rng, num_nodes=num_nodes, feature_ndim=input_channels
-        )
-        graph.update(
-            edges=[
-                (
-                    src,
-                    dst,
-                    {GraphAttrs.EDGE_GROUND_TRUTH: Annotation.TRUE_POSITIVE},
-                )
-                for src, dst in graph.edges
-            ]
-        )
-        data = dataset_from_graph(graph, mode="sub")[0]
-
-        subgraph = nx.ego_graph(graph, 0)
-        num_nodes = subgraph.number_of_nodes()
-        num_edges = subgraph.number_of_edges()
-        node_x, edge_x = gcn(x=data.x, edge_index=data.edge_index)
-
-        assert node_x.size() == (num_nodes, node_output_classes)
-        assert edge_x.size() == (num_edges, edge_output_classes)
+from conftest import random_image_and_graph
 
 
 @pytest.mark.parametrize(
@@ -123,7 +40,7 @@ class TestFeatureExtractor:
 
         assert torch.equal(vars["image"], image_out)
 
-        for node_id, node_attrs in graph_out.nodes.data():
+        for _, node_attrs in graph_out.nodes.data():
             x, y = node_attrs[GraphAttrs.NODE_X], node_attrs[GraphAttrs.NODE_Y]
             features = node_attrs[GraphAttrs.NODE_FEATURES]
 
