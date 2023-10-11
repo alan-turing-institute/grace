424f14039718fccd399262700be05d110a975b89
KristinaUlicna <kulicna@turing.ac.uk>
2023-10-11 18:50:36 +0100

    Extend property cruncher by relative props

--- grace/models/property_cruncher.py
+++ grace/models/property_cruncher.py
@@ -23,58 +23,62 @@ class EdgePropertyCruncher:
         self.mean_edge_length = None
         self.mean_triangle_area = None
 
-        # TODO: finish up!
-        self.x_mn = None
-        self.y_mn = None
-        self.x_mx = None
-        self.y_mx = None
-
     def universal_edge_properties(self) -> None:
-        """Property calculator for most properties.
+        """Property calculator for edge properties considering its geometry.
+            Each edge is positioned as follows, with respect to neighbours:
 
-        TODO: Include standardisation of the coordinates,
-              and relative length to the edge length.
+                    [north]
+                     (north)
+                       |
+                       |
+                       |
+            [west] - - | - - [east]
+                       |
+                       |
+                       |
+                    [south]
+                     (src)
 
         Notes
         -----
         - Modifies the graph in-place.
-        - Creates GraphAttrs.EDGE_PROPERTIES
+        - Creates GraphAttrs.EDGE_PROPERTIES attribute.
         """
 
-        for src, dst, edge in self.graph.edges(data=True):
+        for south, north, edge in self.graph.edges(data=True):
             edge_attributes = {}
 
-            src_coords = np.array(
+            south_coords = np.array(
                 [
-                    self.graph.nodes[src][GraphAttrs.NODE_X],
-                    self.graph.nodes[src][GraphAttrs.NODE_Y],
+                    self.graph.nodes[south][GraphAttrs.NODE_X],
+                    self.graph.nodes[south][GraphAttrs.NODE_Y],
                 ]
             )
-            dst_coords = np.array(
+            north_coords = np.array(
                 [
-                    self.graph.nodes[dst][GraphAttrs.NODE_X],
-                    self.graph.nodes[dst][GraphAttrs.NODE_Y],
+                    self.graph.nodes[north][GraphAttrs.NODE_X],
+                    self.graph.nodes[north][GraphAttrs.NODE_Y],
                 ]
             )
-            mid_coords = (src_coords + dst_coords) / 2.0
+            mid_coords = (south_coords + north_coords) / 2.0
 
             # Store these raw positions:
-            edge_attributes["src_pos_x_raw"] = src_coords[0]
-            edge_attributes["src_pos_y_raw"] = src_coords[1]
-            edge_attributes["dst_pos_x_raw"] = dst_coords[0]
-            edge_attributes["dst_pos_y_raw"] = dst_coords[1]
+            edge_attributes["south_pos_x_raw"] = south_coords[0]
+            edge_attributes["south_pos_y_raw"] = south_coords[1]
+            edge_attributes["north_pos_x_raw"] = north_coords[0]
+            edge_attributes["north_pos_y_raw"] = north_coords[1]
             edge_attributes["mid_pos_x_raw"] = mid_coords[0]
             edge_attributes["mid_pos_y_raw"] = mid_coords[1]
 
             # edge length (raw):
             edge_length_raw = self.calculate_points_distance(
-                src_coords, dst_coords
+                south_coords, north_coords
             )
             edge_attributes["edge_length_raw"] = edge_length_raw
 
             # edge orientation:
             edge_orientation = self.calculate_angle_with_vertical(
-                src_coords, dst_coords
+                south_coords, north_coords
             )
             edge_attributes["edge_orient_rad"] = edge_orientation[0]
             edge_attributes["edge_orient_deg"] = edge_orientation[1]
@@ -85,11 +89,11 @@ class EdgePropertyCruncher:
                 "east": float("inf"),
             }
 
-            neighbours_src = set(self.graph.neighbors(src))
-            neighbours_dst = set(self.graph.neighbors(dst))
+            neighbours_south = set(self.graph.neighbors(south))
+            neighbours_north = set(self.graph.neighbors(north))
 
             # Find the intersection (common neighbors) between the two sets
-            common_neighbours = neighbours_src.intersection(neighbours_dst)
+            common_neighbours = neighbours_south.intersection(neighbours_north)
             assert len(common_neighbours) > 0
 
             # It can certainly happen that a certain edge forms more than one
@@ -102,7 +106,7 @@ class EdgePropertyCruncher:
                     ]
                 )
                 neighbour_position = self.point_position_relative_to_edge(
-                    src_coords, dst_coords, new_coords
+                    south_coords, north_coords, new_coords
                 )
                 mid_to_point_length_raw = self.calculate_points_distance(
                     mid_coords, new_coords
@@ -111,7 +115,7 @@ class EdgePropertyCruncher:
                     mid_coords, new_coords
                 )
                 triangle_area = self.calculate_triangle_area(
-                    src_coords, dst_coords, new_coords
+                    south_coords, north_coords, new_coords
                 )
 
                 if neighbour_position == "west":
@@ -209,36 +213,75 @@ class EdgePropertyCruncher:
                 dictionary["east_triangle_area_raw"] / self.mean_triangle_area
             )
 
+            # Add length of west/east neighbour to mid point, relative to edge length:
+            dictionary["west_to_mid_length_rel"] = (
+                dictionary["west_to_mid_length_raw"]
+                / dictionary["edge_length_raw"]
+            )
+            dictionary["east_to_mid_length_rel"] = (
+                dictionary["east_to_mid_length_raw"]
+                / dictionary["edge_length_raw"]
+            )
+
             edge[GraphAttrs.EDGE_PROPERTIES] = Properties(
                 properties_dict=dictionary
             )
 
-    def normalise_coordinates(self) -> None:
-        """TODO: Complete!"""
-        pass
+    def add_relative_node_coordinates(self) -> None:
+        """TODO: Not normalised, just relative positions in the graph."""
+        x_coords_raw, y_coords_raw = np.transpose(
+            [
+                [node[GraphAttrs.NODE_X], node[GraphAttrs.NODE_Y]]
+                for _, node in self.graph.nodes(data=True)
+            ]
+        )
+        x_mn, x_mx = np.min(x_coords_raw), np.max(x_coords_raw)
+        y_mn, y_mx = np.min(y_coords_raw), np.max(y_coords_raw)
+
+        for _, _, edge in self.graph.edges(data=True):
+            dictionary = edge[GraphAttrs.EDGE_PROPERTIES].properties_dict
+            old_keys_list = list(dictionary.keys())
+
+            # Add new normalised key-value pairs:
+            for old_key in old_keys_list:
+                if old_key.endswith("_pos_x_raw"):
+                    new_key = old_key.replace("_pos_x_raw", "_pos_x_rel")
+                    new_val = (dictionary[old_key] - x_mn) / (x_mx - x_mn)
+                    dictionary[new_key] = new_val
+                elif old_key.endswith("_pos_y_raw"):
+                    new_key = old_key.replace("_pos_y_raw", "_pos_y_rel")
+                    new_val = (dictionary[old_key] - y_mn) / (y_mx - y_mn)
+                    dictionary[new_key] = new_val
+                else:
+                    pass
+
+            edge[GraphAttrs.EDGE_PROPERTIES] = Properties(
+                properties_dict=dictionary
+            )
 
     def process(self) -> nx.Graph:
         self.universal_edge_properties()
         self.find_average_measure_values()
         self.add_normalised_edge_attributes()
+        self.add_relative_node_coordinates()
         return self.graph
 
     @staticmethod
-    def calculate_points_distance(src_coords, dst_coords) -> float:
-        return np.linalg.norm(src_coords - dst_coords)
+    def calculate_points_distance(south_coords, north_coords) -> float:
+        return np.linalg.norm(south_coords - north_coords)
 
     @staticmethod
     def calculate_angle_with_vertical(
-        src_point, dst_point
+        south_point, north_point
     ) -> tuple[float, float]:
         """Calculate angle between line & vertical plane (in radians)."""
 
         # Calculate the midpoint of the line
-        x_src, y_src = src_point
-        x_dst, y_dst = dst_point
+        x_south, y_south = south_point
+        x_north, y_north = north_point
 
-        x_mid = (x_src + x_dst) / 2.0
-        y_mid = (y_src + y_dst) / 2.0
+        x_mid = (x_south + x_north) / 2.0
+        y_mid = (y_south + y_north) / 2.0
 
         # Calculate the angle using arctan2, measured from the vertical axis
         angle_rad = np.arctan2(x_mid, y_mid)
@@ -249,10 +292,12 @@ class EdgePropertyCruncher:
         return angle_rad, angle_deg
 
     @staticmethod
-    def calculate_triangle_area(src_coords, dst_coords, ver_coords) -> float:
+    def calculate_triangle_area(
+        south_coords, north_coords, ver_coords
+    ) -> float:
         """Calculate the area of a triangle defined by three sets of coordinates."""
-        x1, y1 = src_coords
-        x2, y2 = dst_coords
+        x1, y1 = south_coords
+        x2, y2 = north_coords
         x3, y3 = ver_coords
 
         area = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
@@ -260,7 +305,7 @@ class EdgePropertyCruncher:
         return area
 
     @staticmethod
-    def point_position_relative_to_edge(src_node, dst_node, new_node):
+    def point_position_relative_to_edge(south_node, north_node, new_node):
         """
         Determine whether a new point is to the left or to the right
         of an edge line defined by its source and destination nodes.
@@ -269,15 +314,15 @@ class EdgePropertyCruncher:
             A string indicating the relative position: 'left', 'right'
             or 'collinear' if the point is collinear with the edge line.
         """
-        x_src, y_src = src_node
-        x_dst, y_dst = dst_node
+        x_south, y_south = south_node
+        x_north, y_north = north_node
         x_new, y_new = new_node
 
         # Vector representing the edge line segment
-        edge_vector = np.array([x_dst - x_src, y_dst - y_src])
+        edge_vector = np.array([x_north - x_south, y_north - y_south])
 
         # Vector from the source node to the new point
-        new_point_vector = np.array([x_new - x_src, y_new - y_src])
+        new_point_vector = np.array([x_new - x_south, y_new - y_south])
 
         # Calculate the cross product
         cross_product = np.cross(edge_vector, new_point_vector)
